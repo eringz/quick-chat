dd9e2c5812d83e125048dcbeaeb84eec96e586c5
unknown <ron.garcia.santos@gmail.com>
2025-10-09 09:47:08 +0800

    ready for initial deployment both FE and BE

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import path from 'path';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
 
+// make ready for deployment
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 
 app.listen(PORT, () => console.log(`Running on server port ${PORT}`));

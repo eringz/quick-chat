74e8647a0700da754cf87afca7091e6b28e924ef
unknown <ron.garcia.santos@gmail.com>
2025-10-11 23:45:48 +0800

    Update profile endpoint is created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import path from 'path';
+import cookieParser from 'cookie-parser'; 
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 
+
 const app = express();
 const PORT = ENV.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
 

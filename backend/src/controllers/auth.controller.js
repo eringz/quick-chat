88cfe070f4cfbb631b05e90cbdf472a88a9ce087
unknown <ron.garcia.santos@gmail.com>
2025-10-11 20:10:52 +0800

    Signup test

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/User.js';
 import { generateToken } from '../lib/utils.js';
 import bcrypt from 'bcryptjs';
+import 'dotenv/config';
+
 
 export const signup = async (req, res) => {   
     const { firstName, lastName, email, password }  = req.body;
@@ -69,6 +71,13 @@ export const signup = async (req, res) => {
                 email: newUser.email,
                 profilePic: newUser.profilePic,
             });
+
+            // Send a welcome email to user
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.firstName, process.env.CLIENT_URL )
+            } catch (error) {
+                console.error('Failed to send welcome email:', error);
+            }
 
         } else {
             res.status(400).json({message: 'Invalid User data'})

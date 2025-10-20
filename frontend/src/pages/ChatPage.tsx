a03cd3e73f53cd48f1c73bbce9d1380018748b69
unknown <ron.garcia.santos@gmail.com>
2025-10-21 03:20:52 +0800

    mobile friendly test

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
 import ProfileHeader from "../components/ProfileHeader";
 import ActiveTabSwitch from "../components/ActiveTabSwitch";
@@ -10,12 +10,25 @@ import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 const ChatPage: React.FC = () => {
   const { activeTab, selectedUser } = useChatStore();
+  const [isMobile, setIsMobile] = useState(false);
 
+  useEffect(() => {
+    const handleSize = () => setIsMobile(window.innerWidth < 768);
+    handleSize();
+
+    window.addEventListener("resize", handleSize);
+    return () => window.removeEventListener("resize", handleSize);
+  }, []);
+
+  const showLeftSide = !isMobile || (isMobile && !selectedUser);
+  const showRightSide = !isMobile || (isMobile && selectedUser);
   return (
 	<div className="relative w-full max-w-6xl h-[800px]">
-	  <BorderAnimatedContainer>
+	  <BorderAnimatedContainer className="">
       {/**Left Side */}
-      <div className="w-80 bg-slate-800/50 backdrop-blur-sm">
+      {/* <div className="w-80 bg-slate-800/50 backdrop-blur-sm"> */}
+      <div className={`w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col transition-all ease-in duration-1000 ${showLeftSide ? "block" : "hidden"}`}>
+      {/* <div className={`w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col transition-all duration-1000 ${showLeftSide ? "translate-x-1/2" : "translate-x-0"}`}> */}
         <ProfileHeader />
         <ActiveTabSwitch />
         <div className="flex-1 overflow-y-auto p-4 space-y-2">
@@ -24,7 +37,9 @@ const ChatPage: React.FC = () => {
       </div>
 
       {/** Right Side */}
-      <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+      {/* <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm"> */}
+      <div className={`flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm transition-all duration-1000 ease-in-out ${showRightSide ? "flex" : "hidden"}`}>
+      {/* <div className={`flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm transition-all duration-1000 ${showRightSide ? "translate-x-0" : "translate-x-full"}`}> */}
         {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
       </div>
     </BorderAnimatedContainer>
